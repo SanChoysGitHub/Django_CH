186623ec2c6e3b006c60860f12c0444080297694
SanChoys <kotlyrov.oleksandr.04@gmail.com>
2021-04-02 14:34:53 +0300

    Templates

--- mysite/news/views.py
+++ mysite/news/views.py
@@ -1,10 +1,16 @@
 from django.shortcuts import render
 from django.http.response import HttpResponse
 
+from .models import News
+
 
 def index(request):
-    # print(dir(request))
-    return HttpResponse('Hello world')
+    news = News.objects.order_by('-created_at')
+    context = {
+        'news': news,
+        'title': 'News list'
+            }
+    return render(request, template_name='news/index.html', context=context)
 
 
 def test(request):
