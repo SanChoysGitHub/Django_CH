5906b6f26e002734de7e142a92646327c0a2fe73
SanChoys <kotlyrov.oleksandr.04@gmail.com>
2021-04-02 20:21:31 +0300

    Admin

--- mysite/news/admin.py
+++ mysite/news/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import News
+
+
+# Отображение в админке название полей
+class NewsAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title', 'created_at', 'updated_at', 'is_published')
+    list_display_links = ('id', 'title')  # Какие поля будут с ссылкой на объект
+    search_fields = ('title', 'content')
+
+
+admin.site.register(News, NewsAdmin)
+
+
+
--- mysite/news/models.py
+++ mysite/news/models.py
@@ -2,12 +2,17 @@ from django.db import models
 
 
 class News(models.Model):
-    title = models.CharField(max_length=250)
-    content = models.TextField(blank=True)
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
-    photo = models.ImageField(upload_to='photos/%Y/%m/%d/')
-    is_published = models.BooleanField(default=True)
+    title = models.CharField(max_length=250, verbose_name='Name')
+    content = models.TextField(blank=True, verbose_name='Content')
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name='Data of publication')
+    updated_at = models.DateTimeField(auto_now=True, verbose_name='Updated')
+    photo = models.ImageField(upload_to='photos/%Y/%m/%d/', verbose_name='Photo', blank=True)
+    is_published = models.BooleanField(default=True, verbose_name='Published')
 
     def __str__(self):
         return self.title
+
+    class Meta:
+        verbose_name = 'News'
+        verbose_name_plural = 'News'
+        ordering = ['-created_at']
