4e7d2c9b6684374fe93e1db5ff06e4ca09ab64af
SanChoys <kotlyrov.oleksandr.04@gmail.com>
2021-04-02 21:51:51 +0300

    Models

--- mysite/news/admin.py
+++ mysite/news/admin.py
@@ -1,16 +1,28 @@
 from django.contrib import admin
 
-from .models import News
+from .models import News, Category
 
 
 # Отображение в админке название полей
 class NewsAdmin(admin.ModelAdmin):
-    list_display = ('id', 'title', 'created_at', 'updated_at', 'is_published')
+    # Акуратно, это все долбанные кортежи
+    list_display = ('id', 'title', 'category','created_at', 'updated_at', 'is_published')
     list_display_links = ('id', 'title')  # Какие поля будут с ссылкой на объект
     search_fields = ('title', 'content')
+    list_editable = ('is_published',)
+    list_filter = ('is_published', 'category')
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    # Акуратно, это все долбанные кортежи
+    list_display = ('id', 'title')
+    list_display_links = ('id', 'title')
+    search_fields = ('title',)
 
 
 admin.site.register(News, NewsAdmin)
+admin.site.register(Category, CategoryAdmin)
+
 
 
 
--- mysite/news/models.py
+++ mysite/news/models.py
@@ -2,12 +2,13 @@ from django.db import models
 
 
 class News(models.Model):
-    title = models.CharField(max_length=250, verbose_name='Name')
+    title = models.CharField(max_length=150, verbose_name='Name')
     content = models.TextField(blank=True, verbose_name='Content')
     created_at = models.DateTimeField(auto_now_add=True, verbose_name='Data of publication')
     updated_at = models.DateTimeField(auto_now=True, verbose_name='Updated')
     photo = models.ImageField(upload_to='photos/%Y/%m/%d/', verbose_name='Photo', blank=True)
     is_published = models.BooleanField(default=True, verbose_name='Published')
+    category = models.ForeignKey('Category', on_delete=models.PROTECT, null=True, verbose_name='Category')
 
     def __str__(self):
         return self.title
@@ -16,3 +17,15 @@ class News(models.Model):
         verbose_name = 'News'
         verbose_name_plural = 'News'
         ordering = ['-created_at']
+
+
+class Category(models.Model):
+    title = models.CharField(max_length=150, db_index=True, verbose_name='Name')
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = 'Category'
+        verbose_name_plural = 'Categories'
+        ordering = ['title']
