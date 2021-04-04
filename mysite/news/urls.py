825358f048b0d77d4a5a3993e803f09fa0d89867
SanChoys <kotlyrov.oleksandr.04@gmail.com>
2021-04-04 22:07:56 +0300

    Route names

--- mysite/news/urls.py
+++ mysite/news/urls.py
@@ -3,8 +3,8 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path('', index),
-    path('category/<int:category_id>/', get_category),
+    path('', index, name='home'),
+    path('category/<int:category_id>/', get_category, name='category'),
 ]
 
 
